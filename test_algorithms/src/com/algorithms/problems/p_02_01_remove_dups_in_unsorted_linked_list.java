57a15b2b75667c1aa6319ac774841dc19f3818f3
realarkady <realarkady@yahoo.com>
2014-02-08 21:48:18 -0500

    wk

---
+++ test_algorithms/src/com/algorithms/problems/p_02_01_remove_dups_in_unsorted_linked_list.java
@@ -0,0 +1,51 @@
+package com.algorithms.problems;
+
+import com.algorithms.datastructures.generic.LinkedList;
+import com.algorithms.datastructures.generic.Node;
+
+public class p_02_01_remove_dups_in_unsorted_linked_list {
+	
+	public p_02_01_remove_dups_in_unsorted_linked_list() {	}
+	
+	public static void main(String[] args) {
+		LinkedList<Integer> l = new LinkedList<Integer>();
+		for (int i = 0; i < 15; i++) {
+			int n = (int) (Math.random() * 100);
+			l.addToTail(n);
+		}
+		System.out.println(l.toString());
+		removeDuplicates(l);
+		System.out.println(l.toString());
+	}
+	
+	public static void removeDuplicates(LinkedList<Integer> lList){
+		if(lList.head == null)
+			return;
+		
+		if(lList.head.next == null)
+			return; 
+		
+		Node<Integer> previousNode = lList.head;
+		Node<Integer> currentNode = previousNode.next;
+		
+		while(currentNode != null){
+			Node<Integer> runningNode = lList.head;
+			while(runningNode != currentNode){
+				if(runningNode.value == currentNode.value){
+					System.out.println("duplicate value found: " + runningNode.value); 
+					Node<Integer> temp = currentNode.next;
+					previousNode.next = temp;
+					currentNode = temp;
+					break;
+				}
+				runningNode = runningNode.next;
+			}
+			if(runningNode == currentNode){
+				previousNode = currentNode;
+				currentNode = runningNode.next;
+			}
+		}
+		
+	}
+	
+}
--- test_algorithms/src/com/algorithms/problems/p_02_02_nth_to_last_elem_linked_list.java
+++ test_algorithms/src/com/algorithms/problems/p_02_02_nth_to_last_elem_linked_list.java
@@ -4,9 +4,9 @@ import com.algorithms.datastructures.generic.LinkedList;
 import com.algorithms.datastructures.generic.Node;
 
 public class p_02_02_nth_to_last_elem_linked_list {
-	
+
 	public p_02_02_nth_to_last_elem_linked_list() {	}
-	
+
 	public static void main(String[] args) {
 		LinkedList<Integer> l = new LinkedList<Integer>();
 		for (int i = 0; i < 15; i++) {
@@ -15,7 +15,7 @@ public class p_02_02_nth_to_last_elem_linked_list {
 		}
 		System.out.println(l.toString());
 		
-		System.out.println(nThToLast(l, 15));
+		System.out.println(nThToLast(l, 5));
 	}
 	
 	public static Node<Integer> nThToLast(LinkedList<Integer> lList, int nThToLast){
@@ -25,11 +25,17 @@ public class p_02_02_nth_to_last_elem_linked_list {
 		if(lList.head.next == null)
 			return lList.head; 
 		
-		Node<Integer> node = lList.head;
+		Node<Integer> currentNode = lList.head;
+		Node<Integer> backNode = lList.head;
 		
-		while(node.next != null){
-			
-			node = node.next;
+		while(currentNode != null){
+			while(backNode.next != currentNode){
+				if(backNode.next.value == currentNode.value){
+					backNode.next = backNode.next.next;
+				}
+				backNode = backNode.next;
+			}
+			currentNode = currentNode.next;
 		}
 		
 		return null;
