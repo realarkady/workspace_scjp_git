6e8731fb7df827d55d7044c7e9cd4e162f658d5b
realarkady <realarkady@yahoo.com>
2014-02-10 07:44:40 -0500

    hm

--- test_algorithms/src/com/algorithms/sort/QuickSort.java
+++ test_algorithms/src/com/algorithms/sort/QuickSort.java
@@ -5,18 +5,21 @@ public class QuickSort {
 	public static void main(String[] args) {
 		Integer[] a = { 5, 1, 44, 15, 88, 65, 31, 4, 75, 11, 6 };
 		int[] arr = { 5, 1, 44, 15, 88, 65, 31, 4, 75, 11, 6 };
-		SortUtility.show(a);
-		//sort(a);
-		SortUtility.show(a);
-		quickSort(arr);
-		for (int i = 0; i < arr.length; i++) {
+		//SortUtility.show(a);
+		//SortUtility.show(a);
+		
+		for (int i = 0; i < arr.length; i++)
 			System.out.print(arr[i] + " ");
-		}
+		quickSort(arr);
+		System.out.println();
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+		
 	}
 
 	public static void quickSort(int[] a) {
-		int i = a.length / 2;
-		System.out.println(i);
+		//int i = a.length / 2;
+		//System.out.println(i);
 		quickSort(a, 0, a.length-1);
 	}
 
@@ -24,21 +27,32 @@ public class QuickSort {
 		if(first >= last)
 			return;
 		
-		int pivot = a[first];
+		int pivot = a[first + (last - first)/2];
 		int i = first;
 		int j = last - 1;
 		
-		while(i<j){
+		while(i<=j){
 			while(i<pivot){
 				i++;
 			}
 			
 			while(j>pivot){
-				j++;
+				j--;
 			}
 			
+			if(i <= j){
+				exchange(a, i, j);
+				i++;
+				j--;
+			}
 			
 		}
+		
+		if(first < j)
+			quickSort(a, first, j);
+		
+		if(i < last)
+			quickSort(a, i, last);
 		
 	}
 	
