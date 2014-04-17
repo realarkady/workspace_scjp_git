9ab460e26297db788b7ded6d36fa8473a57250f0
barcadie <bacalu.arcadie@gmail.com>
2014-04-17 16:54:01 -0400

    wk

--- scjp_ch7/src/com/test/collections/SortUsingCollections.java
+++ scjp_ch7/src/com/test/collections/SortUsingCollections.java
@@ -1,40 +1,57 @@
 package com.test.collections;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
 
 public class SortUsingCollections {
 	
 	public SortUsingCollections() {	}
 	
 	public static void main(String[] args) {
-		SortableObject so = new SortableObject(1, "name_1");
-		List<SortableObject> sol = new ArrayList<SortableObject>();
-		sol.add(so);
-		sol.add(new SortableObject(3, "name_3"));
-		sol.add(new SortableObject(5, "name_5"));
-		sol.add(new SortableObject(2, "name_2"));
-		sol.add(new SortableObject(1, "name_1"));
-		sol.add(new SortableObject(9, "name_9"));
-		sol.add(new SortableObject(6, "name_6"));
+		MyObject mo = new MyObject(1, "name_1");
+		List<MyObject> mol = new ArrayList<MyObject>();
+		mol.add(mo);
+		mol.add(new MyObject(3, "name_3"));
+		mol.add(new MyObject(5, "name_5"));
+		mol.add(new MyObject(2, "name_2"));
+		mol.add(new MyObject(1, "name_1"));
+		mol.add(new MyObject(9, "name_9"));
+		mol.add(new MyObject(6, "name_6"));
 		
-		Collections.sort(sol);
+		MyObjectComparatorAsc mocAsc = new MyObjectComparatorAsc();
+		MyObjectComparatorDesc mocDesc = new MyObjectComparatorDesc();
 		
-		System.out.println(sol);
+		Collections.sort(mol);
+		System.out.println(mol);
+		
+		Collections.sort(mol, mocDesc);
+		System.out.println(mol);
+		
+		
+		MyObject[] moa = mol.toArray(new MyObject[mol.size()]); //new MyObject[mol.size()];
+		Arrays.sort(moa, mocAsc);
+		for (MyObject myObject : moa) {
+			System.out.println("myObject: " + myObject);
+		}
 		
 	}
 	
 	
 }
 
-class SortableObject implements Comparable<SortableObject>{
+class MyObject implements Comparable<MyObject>{
 	private Integer id;
 	private String name;
 	
-	public SortableObject(){ }
+	public MyObject(){ }
 	
-	public SortableObject(int id, String name) {
+	public MyObject(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -57,13 +74,31 @@ class SortableObject implements Comparable<SortableObject>{
 	}
 
 	@Override
-	public int compareTo(SortableObject so) {
-		return id.compareTo(so.getId());
+	public int compareTo(MyObject mo) {
+		return Integer.valueOf(name.length()).compareTo(mo.getName().length());
 	}
 
 	@Override
 	public String toString() {
 		return "\n{ id: " + id +", name: " + name + " }";
+	}
+	
+}
+
+class MyObjectComparatorAsc implements Comparator<MyObject>{
+
+	@Override
+	public int compare(MyObject o1, MyObject o2) {
+		return o1.getName().compareTo(o2.getName());
+	}
+	
+}
+
+class MyObjectComparatorDesc implements Comparator<MyObject>{
+
+	@Override
+	public int compare(MyObject o1, MyObject o2) {
+		return o2.getName().compareTo(o1.getName());
 	}
 	
 }
